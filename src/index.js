f14987638bb9fbb32ad6633647bdcaff006e6e86
Nathaniel Griswold <nategriswold@gmail.com>
2021-09-17 20:09:12 -0500

    change hardcoded server

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const {Elm} = require('./Main');
 Elm.Main.init({
   flags: {
     elasticsearchMappingSchemaVersion: parseInt(process.env.ELASTICSEARCH_MAPPING_SCHEMA_VERSION) || 0,
-    elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'https://nixos-search-5886075189.us-east-1.bonsaisearch.net:443',
-    elasticsearchUsername : process.env.ELASTICSEARCH_USERNAME || 'z3ZFJ6y2mR',
-    elasticsearchPassword : process.env.ELASTICSEARCH_PASSWORD || 'ds8CEvALPf9pui7XG'
+    elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'http://nixos-search.nan.sh:9200' || 'https://nixos-search-5886075189.us-east-1.bonsaisearch.net:443',
+    elasticsearchUsername : process.env.ELASTICSEARCH_USERNAME || 'client',
+    elasticsearchPassword : process.env.ELASTICSEARCH_PASSWORD || '@Sfrn$BDDw43Yv'
   }
 });
